b013716b61c768a81c0144bd84322c8b656db968
saumensd <saumensutradhar@gmail.com>
2024-09-22 13:26:32 -0700

    alert added

--- js/index.js
+++ js/index.js
@@ -26,20 +26,48 @@ document.getElementById('donation1').addEventListener('click',function(){
     const inputMoney1 = document.getElementById('input1');
     const inputMoney1Value = inputMoney1.value;
     const inputMoney1ValueParseInt = parseInt(inputMoney1Value);
+    if(inputMoney1ValueParseInt <1 || isNaN(inputMoney1ValueParseInt)){
+        alert('Enter a valid Number');
+        inputMoney1.value = '';
+    }
+    else{
+        const myAmmount = document.getElementById('my-ammount');
+        const myAmmountInnerText = myAmmount.innerText;
+        const myAmmountInnerTextParseInt = parseInt(myAmmountInnerText)
+    
+        const donationAmount = document.getElementById('donation-ammount');
+        const donationAmountInnerText = donationAmount.innerText;
+        const donationAmountInnerTextParseInt = parseInt(donationAmountInnerText);
+    
+        const totalDonationAmount =  donationAmountInnerTextParseInt + inputMoney1ValueParseInt ;
+        donationAmount.innerText = totalDonationAmount;
+    
+        const removeMyDonationAmountFromMyAmmount = myAmmountInnerTextParseInt - inputMoney1ValueParseInt;
+        myAmmount.innerText = removeMyDonationAmountFromMyAmmount;
+    
+        inputMoney1.value = '';
+    }
 
-    const myAmmount = document.getElementById('my-ammount');
-    const myAmmountInnerText = myAmmount.innerText;
-    const myAmmountInnerTextParseInt = parseInt(myAmmountInnerText)
-
-    const donationAmount = document.getElementById('donation-ammount');
-    const donationAmountInnerText = donationAmount.innerText;
-    const donationAmountInnerTextParseInt = parseInt(donationAmountInnerText);
-
-    const totalDonationAmount =  donationAmountInnerTextParseInt + inputMoney1ValueParseInt ;
-    donationAmount.innerText = totalDonationAmount;
-
-    const removeMyDonationAmountFromMyAmmount = myAmmountInnerTextParseInt - inputMoney1ValueParseInt;
-    myAmmount.innerText = removeMyDonationAmountFromMyAmmount;
-
-    inputMoney1.value = '';
+  
 })
+// document.getElementById('donation1').addEventListener('click',function(){
+//     const inputMoney1 = document.getElementById('input1');
+//     const inputMoney1Value = inputMoney1.value;
+//     const inputMoney1ValueParseInt = parseInt(inputMoney1Value);
+
+//     const myAmmount = document.getElementById('my-ammount');
+//     const myAmmountInnerText = myAmmount.innerText;
+//     const myAmmountInnerTextParseInt = parseInt(myAmmountInnerText)
+
+//     const donationAmount = document.getElementById('donation-ammount');
+//     const donationAmountInnerText = donationAmount.innerText;
+//     const donationAmountInnerTextParseInt = parseInt(donationAmountInnerText);
+
+//     const totalDonationAmount =  donationAmountInnerTextParseInt + inputMoney1ValueParseInt ;
+//     donationAmount.innerText = totalDonationAmount;
+
+//     const removeMyDonationAmountFromMyAmmount = myAmmountInnerTextParseInt - inputMoney1ValueParseInt;
+//     myAmmount.innerText = removeMyDonationAmountFromMyAmmount;
+
+//     inputMoney1.value = '';
+// })
