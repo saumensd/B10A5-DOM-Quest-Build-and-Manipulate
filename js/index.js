e73b59a7e175f4bc6b3a1480730589cfaf22eddb
saumensd <saumensutradhar@gmail.com>
2024-09-22 12:27:50 -0700

    active button added

--- js/index.js
+++ js/index.js
@@ -2,3 +2,21 @@
 document.getElementById('blog-page-button').addEventListener('click', function(){
     window.location.href = '/blog.html';
 })
+// active donation button
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+function resetActiveButtons() {
+    button1.classList.remove('bg-green-300');
+    button2.classList.remove('bg-green-300');
+}
+
+button1.addEventListener('click', function() {
+    resetActiveButtons(); 
+    button1.classList.add('bg-green-300'); 
+});
+
+button2.addEventListener('click', function() {
+    resetActiveButtons(); 
+    button2.classList.add('bg-green-300'); 
+});
